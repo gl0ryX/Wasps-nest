ca2d09b8b0ab4650492f7ac7396e6f4f1809a98e
gl0ryX <sup3rcc@gmail.com>
2017-04-09 08:59:26 +0300

    Update pings.sh

--- pings.sh
+++ pings.sh
@@ -21,4 +21,4 @@ echo -e "\e[1;32m:::::::Ping Sweep:::::::.\e[0m"
 echo  "Enter Ip: Example 10.15.2"
 read ip
 for i in `seq 1 255`; do ping -c1 $ip.$i; done | grep "bytes from";
-./headerp.sh
+/usr/share/wasp/headerp.sh
