548768d789934eb03e30f9b119f56b76f401bec8
gl0ryX <sup3rcc@gmail.com>
2017-04-09 09:07:42 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -24,13 +24,15 @@ EOF
 echo
 echo -e "\e[1;31m:::::::Wasps Nest Tool v.0 2017:::::::.\e[0m"
 echo "                                              c0ded by sup3r."
-mkdir -p report
+mkdir -p /usr/share/wasp/report
 echo
 echo -e "\e[1;32m1. Information Gathering.\e[0m"
 echo -e "\e[1;32m2. Nmap ninja c0mmands.\e[0m"
 echo -e "\e[1;32m3. ScanFu.\e[0m"
-echo -e "\e[1;32m4. Metasploit Payloads.\e[0m"
-echo -e "\e[1;32m5. Exit.\e[0m"
+echo -e "\e[1;32m4. Database Assessment .\e[0m"
+echo -e "\e[1;32m5. LFI Attack.\e[0m"
+echo -e "\e[1;32m6. Metasploit Payloads.\e[0m"
+echo -e "\e[1;32m7. Exit.\e[0m"
 echo
 echo -e "\e[1;36m1. Choice.\e[0m"
 read choice
@@ -38,10 +40,13 @@ read choice
 echo $choice
 
 case $choice in
-     1) ./infogathering.sh ;;
-     2) ./nmapninja.sh ;;
-     3) ./scanfu.sh ;;
-     4) ./payloads.sh ;;
-     5) exit ;;
+     1) /usr/share/wasp/infogathering.sh ;;
+     2) /usr/share/wasp/nmapninja.sh ;;
+     3) /usr/share/wasp/scanfu.sh ;;
+     4) /usr/share/wasp/dbassessmnt.sh ;;
+     5) /usr/share/wasp/curlwsp.sh ;;
+     6) /usr/share/wasp/payloads.sh ;;
+     7) exit ;;
      *) echo; echo "Invalid choice."; echo
 esac
+
