42cd360adcc476f421cf7c1d2affd492960310ed
gl0ryX <sup3rcc@gmail.com>
2017-10-17 09:48:54 +0300

    Update nmapninja.sh

--- nmapninja.sh
+++ nmapninja.sh
@@ -26,26 +26,32 @@ EOF
 echo
 echo -e "\e[1;31mPlease enter Target Ip:\e[0m"
 read ip
-
+while :
+do
 echo "[*]-1 Brute forcing SMTP passwords"
 echo "[*]-2 Xmas Scan" 
 echo "[*]-3 Nmap Null Scan" 
 echo "[*]-4 Nmap scan all port" 
 echo "[*]-5 Nmap scan port open" 
 echo "[*]-6 Back" 
+echo "[*]-7 exit" 
 echo
 echo " - - - - "$ip" - - - -"
 echo -e "\e[1;36m1. Choice.\e[0m"
-read choice
+read -n1 -s
 
 echo $choice
 
-case $choice in
+case "$REPLY" in
      1) nmap -p25 -Pn –script smtp-brute $ip ;;
      2) nmap -sX -v $ip ;;
      3) nmap -sN -V $ip ;;
      4) nmap -A -p- $ip ;;
      5) nmap -A -p- -Pn --open -T4 $ip ;;
      6) wasp ;;
+     7) exit ;;
      *) echo; echo "Invalid choice."; echo
 esac
+sleep 1
+
+done
