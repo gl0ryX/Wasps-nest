35d697ab913bb12504ceb362127d9035f0481717
gl0ryX <sup3rcc@gmail.com>
2017-10-17 09:53:18 +0300

    Update headerp.sh

--- headerp.sh
+++ headerp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo
 echo
-echo -e "\e[1;31m:::::::Wasps Nest Tool v.0 2017:::::::.\e[0m"
+echo -e "\e[1;31m:::::::Wasp Tool v.1 2017:::::::.\e[0m"
 echo
 echo -e "\e[1;32m1. Ping Sweep "
 echo -e "\e[1;32m2. Back"
