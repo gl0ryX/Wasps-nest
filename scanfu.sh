8ef1d0e202a8188e41d968c8c6ac41c02d2cd00a
gl0ryX <sup3rcc@gmail.com>
2017-10-17 09:50:48 +0300

    Update scanfu.sh

--- scanfu.sh
+++ scanfu.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# Wasps Nest Tool v1 beta
-# this tool work on kali linux .  
-# 
-#
-#
 echo
 cat << "EOF"
 
@@ -15,29 +10,36 @@ __      ____ _ ___ _ __
                   |_| 
 EOF
 echo
-
+echo -e "\e[1;31m:::::::Wasp Tool v.1 2017:::::::.\e[0m"
 echo -e "\e[1;92m:::::::ScanFu:::::::.\e[0m"
 echo -e "\e[1;32mPlease enter Target Ip:\e[0m"
 read ip
 
+while : 
+do
 echo
 echo -e "\e[1;32m[1]. nikto.\e[0m"
 echo -e "\e[1;32m[2]. Wordpress Scan.\e[0m"
 echo -e "\e[1;32m[3]. Joomla Scan.\e[0m"
 echo -e "\e[1;32m[4]. Fuzzing Files[wfuzz].\e[0m"
 echo -e "\e[1;32m[5]. Back.\e[0m"
+echo -e "\e[1;32m[6]. Exit.\e[0m"
 echo
-echo " - - - - "$ip" - - - -"s
+echo " - - - - "$ip" - - - -"
 echo -e "\e[1;36m1. Choice.\e[0m"
-read choice
+read -n1 -s
 
 echo $choice
 
-case $choice in
+case "$REPLY" in
      1) nikto -h $ip  ;;
      2) wpscan --url $ip  ;;
      3) joomscan -u $ip  ;;
      4) wfuzz --hc 404 -w /usr/share/seclists/Discovery/Web_Content/big.txt $ip/FUZZ ;;
      5) wasp ;;
+     6) exit ;;
      *) echo; echo "Invalid choice."; echo
 esac
+sleep 1 
+done
+
