12d27b110619efea10821656de724758163b5091
gl0ryX <sup3rcc@gmail.com>
2017-10-18 09:49:43 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-cp main.sh /usr/bin/wasp
+cp /usr/share/wasp/main.sh /usr/bin/wasp
 chmod +x /usr/bin/wasp
-chmod 755 * 
+chmod 755 /usr/share/wasp/* 
+
+
+echo -e "\e[1;32m1. run ./wasp .\e[0m"
 
 
